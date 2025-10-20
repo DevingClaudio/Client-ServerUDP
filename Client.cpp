9a58e95307fb95383cfa99b8196f1d60d4832db0
DevingClaudio <claudio.molicafranco@gmail.com>
2025-10-20 13:45:31 +0200

    Ended

---
+++ Client.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // 1. Crea la socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Errore nella creazione della socket");
+        return 1;
+    }
+
+    // 2. Imposta i parametri del server
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+
+    // 3. Invia messaggio
+    std::string messaggio;
+    std::cout << "Inserisci messaggio da inviare: ";
+    std::getline(std::cin, messaggio);
+
+    sendto(sockfd, messaggio.c_str(), messaggio.size(), 0,
+           (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    // 4. Ricevi risposta (opzionale)
+    socklen_t len = sizeof(server_addr);
+    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                     (struct sockaddr*)&server_addr, &len);
+    buffer[n] = '\0';
+    std::cout << "Risposta dal server: " << buffer << std::endl;
+
+    close(sockfd);
+    return 0;
+}
--- Server.cpp
+++ Server.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
 #include <cstring>
-#include <arpa/inet.h>   
+#include <arpa/inet.h>   // per inet_ntoa, htons, ecc.
 #include <sys/socket.h>
-#include <unistd.h>      
-using namespace std;
+#include <unistd.h>      // per close()
 
-#define porta 8080
-#define Buffer_Size 1024
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-char buffer[Buffer_Size];
-struct sockaddr_in serverAddr, clientAddr;
-socklen_t clientLen = sizeof(clientAddr);
-int Socket = socket(AF_INET, SOCK_DGRAM, 0);
-if (Socket < 0) {
-    cout<<"Errore nella creazione del socket";
-    return -1;
-}
-else {
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);  
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
 
-    if (bind(Socket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        cout<<"Errore nel binding a indirizzo e porta"
+    // 1. Creazione socket UDP
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Errore nella creazione della socket");
+        return 1;
     }
-    else {
-        cout<<'Server UDP in ascolto sulla porta: '<<PORT<<'\n';
-        while (true) {
-            memset(buffer, 0, Buffer_Size)
-            
+
+    // 2. Imposta i parametri del server
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;        // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);      // Porta in ascolto
+
+    // 3. Bind socket alla porta
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Errore nel bind");
+        close(sockfd);
+        return 1;
+    }
+
+    std::cout << "Server UDP in ascolto sulla porta " << PORT << "...\n";
+
+    // 4. Loop per ricevere messaggi
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                           (struct sockaddr*)&client_addr, &client_len);
+        if (len < 0) {
+            perror("Errore nella ricezione");
+            break;
         }
+
+        std::cout << "Messaggio ricevuto da " 
+                  << inet_ntoa(client_addr.sin_addr) 
+                  << ": " << buffer << std::endl;
+
+        // (opzionale) Risposta al client
+        std::string risposta = "Ricevuto: " + std::string(buffer);
+        sendto(sockfd, risposta.c_str(), risposta.size(), 0,
+               (struct sockaddr*)&client_addr, client_len);
     }
+
+    close(sockfd);
+    return 0;
 }
